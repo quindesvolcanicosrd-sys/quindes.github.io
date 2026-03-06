dbf833f3cf8933d1950235ffd1039025530315f1
quindesvolcanicosrd-sys <quindesvolcanicosrd@gmail.com>
2026-03-06 13:46:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 // ── CONFIGURACIÓN ── reemplazá estos valores ──────────────────
 const CONFIG = {
   // Tu GAS Web App URL (termina en /exec)
-  GAS_URL: 'https://script.google.com/macros/s/TU_DEPLOYMENT_ID/exec', // ← reemplazar con la URL del nuevo deploy,
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbwr8wttXc58SEoQYdUnefBVEe6rVTp69W03iBfwweugQnn_sCLpDoFlNLaVkfmKTD3I/exec', // ← reemplazar con la URL del nuevo deploy,
   // Tu Google OAuth Client ID (de Google Cloud Console)
   GOOGLE_CLIENT_ID: '190762038083-nlmie46eah0qq5kd5l86fiq3jteg2pr4.apps.googleusercontent.com',
 };
